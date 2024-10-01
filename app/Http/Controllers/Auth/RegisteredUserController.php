060b7ad53be92558b1e3efc8c9efcbd85e983e0a
II-Jaev-II <christiantejano18@gmail.com>
2024-10-01 16:41:37 +0800

    Added the login and registration form

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -31,13 +31,16 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
+            'username' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
             'name' => $request->name,
+            'userName' => $request->username,
             'email' => $request->email,
+            'userType' => $request->userType,
             'password' => Hash::make($request->password),
         ]);
 
